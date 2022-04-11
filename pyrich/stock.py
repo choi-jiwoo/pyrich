33f5a95f9ac86f2333b5219c921d75eb79cda8b7
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-04-11 16:49:13 +0900

    feat: add getting current stock price in KOR market

--- pyrich/stock.py
+++ pyrich/stock.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup as bs
+import requests
 from pyrich.api import set_finnhub
 
 
@@ -7,3 +9,23 @@ def get_from_us_market(symbol: str):
     price_now = finnhub.quote(symbol)
     return price_now
 
+def get_from_kor_market(symbol: str):
+    info = scrape_from_naver_finance(symbol)
+    data = []
+    for tag in info:
+        item = tag.get_text()
+        item = item.replace(',', '')
+        data.append(float(item))
+    label = ['c', 'd', 'dp']  # c: current price, d: change, dp: percent change
+    price_now = {k: v for k, v in zip(label, data)}
+    return price_now
+
+def scrape_from_naver_finance(symbol: str):
+    url = f'https://finance.naver.com/item/main.nhn?code={symbol}'
+    res = requests.get(url)
+    res.raise_for_status()
+    html = res.text
+    soup = bs(html, 'html.parser')
+    today = soup.select_one('#chart_area > div.rate_info > div')
+    tags = today.find_all('span', class_='blind')
+    return tags
