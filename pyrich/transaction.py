5df39c7de0ea88beff946b6ac30364be93ffae49
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-04-24 16:02:24 +0900

    feat: add method for creating dividend record from user input

--- pyrich/transaction.py
+++ pyrich/transaction.py
@@ -19,3 +19,13 @@ class Transaction:
             raise EmptySymbolError('Symbol is not provided.')
         else:
             self._record = record
+
+    def record_dividends(self, column: list) -> dict:
+        dividend_record = dict.fromkeys(column)
+        items = self.record.items()
+        for item in items:
+            if item[0] in dividend_record:
+                dividend_record[item[0]] = item[1]
+        dividend_record['dividend'] = dividend_record.pop('price')
+        return dividend_record
+
