80590fa941ce9881227dfa17f122cebd22c7ad79
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-04-25 15:29:38 +0900

    feat: add displaying transaction history data from database

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from pyrich.database import PostgreSQL
 
+db = PostgreSQL()
 st.title('Streamlit Application')
+
+# Record history
+st.header('Transaction History')
+transaction_history = db.show_table('transaction')
+st.dataframe(transaction_history)
+
+st.header('Dividend History')
+dividend_history = db.show_table('dividend')
+st.dataframe(dividend_history)
