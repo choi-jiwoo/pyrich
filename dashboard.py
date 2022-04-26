d75f38e7ee80ced66ca6fbcfd91f3e89aaa5284b
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-04-26 12:10:43 +0900

    feat: add option menu in sidebar

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,37 @@
 import streamlit as st
-from pyrich.database import PostgreSQL
+from streamlit_option_menu import option_menu
+from pyrich.portfolio import Portfolio
 
-db = PostgreSQL()
-st.title('Streamlit Application')
+
+portfolio = Portfolio('Choi Ji Woo')
+st.title(f'Hello {portfolio.name} 👋🏼')
 
 with st.sidebar:
-    st.title('Sidebar')
+    # https://icons.getbootstrap.com/
+    selected = option_menu(
+        menu_title='Main Menu',
+        options=[
+            'Dashboard',
+            'Portfolio',
+            'My Asset',
+            'Transaction History',
+            ], 
+        icons=['house', 'stack', 'stack', 'stack'],
+        menu_icon='list',
+        default_index=0
+    )
 
-# Record history
-st.header('Transaction History')
-transaction_history = db.show_table('transaction')
-st.dataframe(transaction_history)
+if selected == 'Dashboard':
+    st.header('Dashboard')    
+elif selected == 'Portfolio':
+    st.header('Portfolio')
+elif selected == 'My Asset':
+    st.header('My Asset')
+elif selected == 'Transaction History':
+    st.header('Transaction History')
+    transaction_history = portfolio.transaction_history('transaction')
+    st.dataframe(transaction_history)
 
-st.header('Dividend History')
-dividend_history = db.show_table('dividend')
-st.dataframe(dividend_history)
+    st.header('Dividend History')
+    dividend_history = portfolio.transaction_history('dividend')
+    st.dataframe(dividend_history)
