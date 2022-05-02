4b084a1addef5dfe285e818e823c6acb955368df
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-02 14:32:46 +0900

    feat: add update_current_cash method

--- pyrich/cash.py
+++ pyrich/cash.py
@@ -7,5 +7,14 @@ class Cash(Record):
     def __init__(self, table: str) -> None:
         super().__init__(table)
 
+    def update_current_cash(self, column: str, value: str, currency: str) -> None:
+        value = float(value)
+        currency_id = {
+            'KRW': 1,
+            'USD': 2,
+        }
+        _id = currency_id[currency]
+        self.db.update('cash', [column], [value], _id, msg=False)
+
     def __repr__(self) -> str:
         return f"Cash(table='{self.table}')"
