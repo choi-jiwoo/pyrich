98da349aeb6ff51dd45549d23dec31042cb0dbe5
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-02 14:32:29 +0900

    feat: add ArrayLengthDoesNotMatchError custom error

--- pyrich/error.py
+++ pyrich/error.py
@@ -2,3 +2,9 @@ class EmptySymbolError(Exception):
 
     def __init__(self, msg: str, *args, **kwargs) -> None:
         super().__init__(msg, *args, **kwargs)
+
+
+class ArrayLengthDoesNotMatchError(Exception):
+
+    def __init__(self, msg: str, *args, **kwargs) -> None:
+        super().__init__(msg, *args, **kwargs)
