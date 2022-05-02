a90ec94d6bd4abb12104873be8fa5baeff3d440a
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-02 14:42:00 +0900

    fix: add handling empty value input

--- pyrich/cash.py
+++ pyrich/cash.py
@@ -8,13 +8,17 @@ class Cash(Record):
         super().__init__(table)
 
     def update_current_cash(self, column: str, value: str, currency: str) -> None:
-        value = float(value)
-        currency_id = {
-            'KRW': 1,
-            'USD': 2,
-        }
-        _id = currency_id[currency]
-        self.db.update('cash', [column], [value], _id, msg=False)
+        try:
+            value = float(value)
+        except ValueError:
+            value = 0
+        finally:
+            currency_id = {
+                'KRW': 1,
+                'USD': 2,
+            }
+            _id = currency_id[currency]
+            self.db.update('cash', [column], [value], _id, msg=False)
 
     def __repr__(self) -> str:
         return f"Cash(table='{self.table}')"
