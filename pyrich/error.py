162ee3a7b9bc084b376df9cb8865d014a62cc76f
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-09 16:18:30 +0900

    feat: add custom exception SearchError

--- pyrich/error.py
+++ pyrich/error.py
@@ -8,3 +8,8 @@ class ArrayLengthDoesNotMatchError(Exception):
 
     def __init__(self, msg: str, *args, **kwargs) -> None:
         super().__init__(msg, *args, **kwargs)
+
+class SearchError(Exception):
+
+    def __init__(self, msg: str, *args, **kwargs) -> None:
+        super().__init__(msg, *args, **kwargs)
