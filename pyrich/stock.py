039bc18f620b388885ba834edcfe8b405b3d5307
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-09 16:19:35 +0900

    feat: add function for retrieving symbol by its name

--- pyrich/stock.py
+++ pyrich/stock.py
@@ -2,7 +2,13 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import requests
 from pyrich.api import set_finnhub
+from pyrich.error import SearchError
 
+HEADERS = {
+    'User-Agent': 'Mozilla',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
 
 def get_current_price(stock: dict) -> float:
     if stock['country'] == 'USA':
@@ -16,6 +22,54 @@ def get_from_us_market(symbol: str):
     finnhub = set_finnhub()
     price_now = finnhub.quote(symbol)
     return price_now
+
+def search_kor_company_symbol(company_name: str) -> tuple:
+    url = 'https://kind.krx.co.kr/common/searchcorpname.do'
+    data = {
+        'method': 'searchCorpNameJson',
+        'searchCodeType': 'char',
+        'searchCorpName': company_name,
+    }
+    res = requests.post(url, headers=HEADERS, data=data)
+    try:
+        res_data = res.json()
+        first_search_res = res_data[0]
+        official_comp_name = first_search_res['repisusrtkornm']
+        comp_symbol = first_search_res['repisusrtcd2']
+        return official_comp_name, comp_symbol
+    except Exception:
+        res.raise_for_status()
+
+def search_us_company_symbol(company_name: str) -> tuple:
+    company_name = 'snowflake'
+    url = 'https://efts.sec.gov/LATEST/search-index'
+    form_data = f'{{"keysTyped": "{company_name}","narrow": true}}'
+    res = requests.post(url, data=form_data)
+    try:
+        res_data = res.json()
+        search_result = res_data['hits']['hits']
+        top_result = search_result[0]
+        stock_info = top_result['_source']
+        official_company_name = stock_info['entity']
+        official_company_name = re.sub(
+            r'\s\(\w*\)',
+            '',
+            official_company_name,
+            flags=re.IGNORECASE
+        )
+        comp_symbol = stock_info['tickers']
+        return official_company_name, comp_symbol
+    except Exception:
+        res.raise_for_status()
+
+def get_symbol(company_name: str, country: str='USA') -> tuple:
+    if country == 'USA':
+        comp = search_us_company_symbol(company_name)
+    elif country == 'KOR':
+        comp = search_kor_company_symbol(company_name)
+    else:
+        raise SearchError('Company name not found.')
+    return comp
 
 def scrape_from_naver_finance(symbol: str):
     url = f'https://finance.naver.com/item/main.nhn?code={symbol}'
