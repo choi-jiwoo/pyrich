8bb2a86ab071fa3522c043952e27f3c694c303cd
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-10 15:48:47 +0900

    style: rename tables to naver_finance_currency_data

--- pyrich/stock.py
+++ pyrich/stock.py
@@ -99,7 +99,7 @@ def get_from_kor_market(symbol: str):
 
 def get_usd_to_krw():
     url = 'https://finance.naver.com/marketindex/exchangeDetail.naver?marketindexCd=FX_USDKRW'
-    tables = pd.read_html(url)
-    currency_table = tables[0]
+    naver_finance_currency_data = pd.read_html(url)
+    currency_table = naver_finance_currency_data[0]
     current_usd_to_krw = currency_table.iloc[0, 0]
     return current_usd_to_krw
