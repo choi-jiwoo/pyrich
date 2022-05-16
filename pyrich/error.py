7ae378da3a84fc49d780e7a8b0fb381dfc7cedb3
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-05-16 10:50:10 +0900

    feat: add custom exception CurrencyError

--- pyrich/error.py
+++ pyrich/error.py
@@ -9,7 +9,14 @@ class ArrayLengthDoesNotMatchError(Exception):
     def __init__(self, msg: str, *args, **kwargs) -> None:
         super().__init__(msg, *args, **kwargs)
 
+
 class SearchError(Exception):
+
+    def __init__(self, msg: str, *args, **kwargs) -> None:
+        super().__init__(msg, *args, **kwargs)
+
+
+class CurrencyError(Exception):
 
     def __init__(self, msg: str, *args, **kwargs) -> None:
         super().__init__(msg, *args, **kwargs)
