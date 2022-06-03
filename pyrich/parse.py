0786b3f7d4f541e9352955916f60e789ef027f34
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-06-03 12:18:42 +0900

    refactor: simplify recording transaction with arguments

--- pyrich/parse.py
+++ pyrich/parse.py
@@ -3,54 +3,23 @@ import argparse
 
 def set_args():
     parser = argparse.ArgumentParser(description="Hello👋🏼 I'm pyrich!")
-    # record transaction date
-    parser.add_argument(
-        '-d',
-        '--date',
-        type=str,
-        default='1900-1-1',
-        metavar='DATE',
-        help='Date of the transaction made. Defaults to 1900-1-1'
-    )
-    # record country
-    parser.add_argument(
-        '-c',
-        '--country',
-        default='USA',
-        type=str,
-        metavar='COUNTRY CODE',
-        help='Country code of where stock is listed. Defaults to USA'
-    )
-    # symbol to record
-    parser.add_argument(
-        '-s',
-        '--symbol',
-        type=str,
-        metavar='SYMBOL',
-        help='Symbol of a stock'
-    )
-    # record transaction type
+    # record transaction
     parser.add_argument(
         '-t',
-        '--type',
+        '--transaction',
         type=str,
-        choices=['buy', 'sell', 'dividend'],
-        metavar='TRANSACTION TYPE',
-        help="Choose among transaction type 'buy', 'sell', 'dividend'"
+        nargs=6,
+        metavar=('DATE', 'COUNTRY', 'TYPE', 'SYMBOL', 'QUANTITY', 'PRICE'),
+        help='Record transaction'
     )
+    # record dividends
     parser.add_argument(
-        '-q',
-        '--quantity',
-        type=int,
-        metavar='QUANTITY',
-        help='Transaction amounts'
-    )
-    parser.add_argument(
-        '-p',
-        '--price',
-        type=float,
-        metavar='PRICE',
-        help='Traded price'
+        '-d',
+        '--dividend',
+        type=str,
+        nargs=4,
+        metavar=('DATE', 'SYMBOL', 'AMOUNT', 'CURRENCY'),
+        help='Record dividends received'
     )
     # copy transaction data from csv file
     parser.add_argument(
