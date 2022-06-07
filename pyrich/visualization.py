052c5d67cbe26affe022afec7bf179fecf4d77f5
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-06-07 14:57:37 +0900

    feat: add drawing stock chart

--- pyrich/visualization.py
+++ pyrich/visualization.py
@@ -16,6 +16,40 @@ def draw_line(data: pd.DataFrame, **kwargs):
     )
     return fig
 
+def draw_stock_chart(close: pd.Series, average_price: float, **kwargs):
+    fig = px.line(close, **kwargs)
+    fig.update_layout(
+        margin=dict(t=0, l=0, r=0, b=0),
+        xaxis=dict(title=None),
+        yaxis=dict(title=None),
+        showlegend=False,
+        shapes=[
+            dict(
+                line=dict(color='gray'),
+                x0=0,
+                x1=1,
+                y0=average_price,
+                y1=average_price,
+                xref='paper',
+                yref='y',
+                line_width=1,
+            )
+        ],
+    )
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label='1m', step='month', stepmode='backward'),
+                dict(count=6, label='6m', step='month', stepmode='backward'),
+                dict(count=1, label='YTD', step='year', stepmode='todate'),
+                dict(count=1, label='1y', step='year', stepmode='backward'),
+                dict(step='all')
+            ])
+        )
+    )
+    return fig
+
 def draw_pie(data: pd.DataFrame, **kwargs):
     fig = px.pie(data, **kwargs)
     return fig
