8b7bcdde0c05698e0a9477b94abb65122af2403c
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-06-30 14:02:45 +0900

    feat: add styling text on terminal

--- pyrich/style.py
+++ pyrich/style.py
@@ -54,3 +54,21 @@ def style_trade_type(_type: str) -> str:
     elif _type == 'sell':
         style = f"color:{color['sell']};"
     return style
+
+def style_terminal_text(text, color: Optional[str]=None, style: Optional[str]=None) -> str:
+    ansi_styles = {
+        None: '',
+        'black': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'magenta': '\033[35m',
+        'cyan': '\033[36m',
+        'white': '\033[37m',
+        'none': '\033[39m',  # reset
+        'end': '\033[0m',  # reset all
+        'bold': '\033[1m',
+    }
+    styled_text = f"{ansi_styles[style]}{ansi_styles[color]}{text}{ansi_styles['end']}"
+    return styled_text
